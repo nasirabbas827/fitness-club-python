1c859f2cd737a28e7ccb2c9f5c03c4ed5f064dc2
NASIR ABBAS <85347981+nasirabbas827@users.noreply.github.com>
2026-06-05 11:38:07 +0500

    security: remove exposed credentials

--- fitnessproject/settings.py
+++ fitnessproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^2$r3s1pp%+cvr+kt3hjo#kr1x!1)i%05kwi#t&_hse#%0yia2'
+SECRET_KEY = "YOUR_OWN_API_KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
